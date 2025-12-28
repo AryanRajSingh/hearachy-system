d89971cff966b97ad32f9f1a6952de93cc8dc926
Aryan Raj <aryanraj844101@gmail.com>
2025-12-28 15:33:20 +0530

    just push again 2nd time  to check where render app run or not

--- db.js
+++ db.js
@@ -1,22 +1,14 @@
 import mysql from "mysql2/promise";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const db = await mysql.createPool({
   host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),   // ✅ THIS WAS MISSING
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-
   ssl: {
-    rejectUnauthorized: false           // ✅ REQUIRED for Aiven
-  },
-
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+    rejectUnauthorized: false
+  }
 });
 
 export default db;
